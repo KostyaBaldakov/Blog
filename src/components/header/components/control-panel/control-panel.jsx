d88f98130425c755230f1195b85f3baecc631b7c
Kostya <baldakov.kostya@yandex.ru>
2024-10-18 19:50:20 +0300

    Create login page, form, validation, input, button, error, forwardRef

--- src/components/header/components/control-panel/control-panel.jsx
+++ src/components/header/components/control-panel/control-panel.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Icon } from "../../../../components";
+import { Icon, Button } from "../../../../components";
 import styled from "styled-components";
 
 const RightAligned = styled.div`
@@ -7,19 +7,8 @@ const RightAligned = styled.div`
   justify-content: flex-end;
 `;
 
-const StyledLink = styled(Link)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  width: 100px;
-  height: 32px;
-  border: 1px solid #000;
-  background-color: #eee;
-`;
-
-const StyledButton = styled.div`
-  &: hover {
+const StyledIcon = styled.div`
+  &:hover {
     cursor: pointer;
   }
 `;
@@ -29,12 +18,14 @@ const ControlPanelConteiner = ({ className }) => {
   return (
     <div className={className}>
       <RightAligned>
-        <StyledLink to="/login">Войти</StyledLink>
+        <Button>
+          <Link to="/login">Войти</Link>
+        </Button>
       </RightAligned>
       <RightAligned>
-        <StyledButton onClick={() => navigate(-1)}>
+        <StyledIcon onClick={() => navigate(-1)}>
           <Icon id="fa-backward" margin="10px 0 0 0" />
-        </StyledButton>
+        </StyledIcon>
         <Link to="/post">
           <Icon id="fa-file-text-o" margin="10px 0 0 16px" />
         </Link>
