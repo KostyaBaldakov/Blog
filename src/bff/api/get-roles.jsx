e8fb6f8d21cadeff668556bdfd1f59e866fca802
Kostya <baldakov.kostya@yandex.ru>
2024-11-12 17:16:27 +0300

    Страница пользователей

---
+++ src/bff/api/get-roles.jsx
@@ -0,0 +1,4 @@
+export const getRoles = () =>
+  fetch("http://localhost:3001/roles").then((loadedRoles) =>
+    loadedRoles.json()
+  );
---
+++ src/pages/users/users.jsx
@@ -0,0 +1,67 @@
+import { H2 } from "../../components";
+import { TableRow, UserRow } from "./components";
+import { useServerRequest } from "../../hooks";
+import { useEffect, useState } from "react";
+import { Content } from "../../components";
+import { ROLE } from "../../constants";
+import styled from "styled-components";
+
+const UsersContainer = ({ className }) => {
+  const [users, setUsers] = useState([]);
+  const [roles, setRoles] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const requestServer = useServerRequest("fetchRoles");
+
+  useEffect(() => {
+    Promise.all([
+      requestServer("fetchUsers"),
+      requestServer("fetchRoles"),
+    ]).then(([usersRes, rolesRes]) => {
+      if (usersRes.error || rolesRes.error) {
+        setErrorMessage(usersRes.error || rolesRes.error);
+        return;
+      }
+
+      setUsers(usersRes.res);
+      setRoles(rolesRes.res);
+    });
+  }, [requestServer]);
+
+  return (
+    <div className={className}>
+      <>
+        <Content error={errorMessage}>
+          <H2>Пользователи</H2>
+          <div>
+            <TableRow>
+              <div className="login-column">Логин</div>
+              <div className="registered-at-column">Дата регистрации</div>
+              <div className="role-column">Роль</div>
+            </TableRow>
+
+            {users.map(({ id, login, registeredAt, roleId }) => (
+              <UserRow
+                key={id}
+                id={id}
+                login={login}
+                registeredAt={registeredAt}
+                roleId={roleId}
+                roles={roles.filter(({ id: roleId }) => +roleId !== ROLE.GUEST)}
+              />
+            ))}
+          </div>
+        </Content>
+      </>
+    </div>
+  );
+};
+
+export const Users = styled(UsersContainer)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  width: 570px;
+  font-size: 18px;
+`;
